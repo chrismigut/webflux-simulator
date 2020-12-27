7eccc020a7111bfb1419c6df9fe638f860382b0d
Chris Migut <cmiguthere@yahoo.com>
2020-12-27 04:17:51 -0500

    first commit

---
+++ src/main/java/services/binarybyte/webfluxsimulatordemo/WebfluxSimulatorDemoApplication.java
@@ -0,0 +1,13 @@
+package services.binarybyte.webfluxsimulatordemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxSimulatorDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebfluxSimulatorDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/services/binarybyte/webfluxsimulatordemo/WebfluxSimulatorDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package services.binarybyte.webfluxsimulatordemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxSimulatorDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
